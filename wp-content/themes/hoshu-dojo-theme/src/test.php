b7814857f98e814ab71e821e5f7c4b81155f49ac
Jeff Kamo <kamojeffrey@gmail.com>
2015-09-22 20:26:58 -0700

    Further style the locations table

---
+++ wp-content/themes/hoshu-dojo-theme/src/test.php
@@ -0,0 +1,179 @@
+<table class="c-location-table">
+    <thead>
+        <tr>
+            <th>Dojo Name</th>
+            <th>Instructor</th>
+            <th>Times</th>
+            <th>Cost</th>
+            <th>Location</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <tr>
+            <td><a href="/">Portland, Hoshu Dojo</a></td>
+            <td>
+                <p>Thomas Groendal</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+        <tr>
+            <td><a href="/">Vancouver, Hoshu Dojo</a></td>
+            <td>
+                <p>Ben Lew</p>
+                <p>Jodo Sandan, Iaido Sandan, Kobujutsu Shodan, Taekwondo Yidan</p>
+                <p>benlew@hoshudojo.com</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Thursdays</td>
+                            <td>8:30 - 10:00pm</td>
+                        </tr>
+                        <tr>
+                            <td>Sundays</td>
+                            <td>12:45 - 2:15pm</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>$15 drop in; $100 per 2 month session</td>
+            <td><p>Vancouver BC, Canada</p><p>Creekside Community Centre</p></td>
+        </tr>
+
+        <tr>
+            <td><a href="/">Seattle &amp; Bellevue, Hoshu Dojo</a></td>
+            <td>
+                <p>Marcus Phung</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+        <tr>
+            <td><a href="http://ewolson.wix.com/tonbo-dojo">Tonbo Dojo, Silverdale</a></td>
+            <td>
+                <p>Ed Olson</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+        <tr>
+            <td><a href="/">Cancún, Hoshu Dojo</a></td>
+            <td>
+                <p>Mauricio Salazar</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+        <tr>
+            <td><a href="/">大分-Oita</a></td>
+            <td>
+                <p>Instructor...</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+        <tr>
+            <td><a href="/">豊州杖道会 – Hoshu Jodokai</a></td>
+            <td>
+                <p>Instructor...</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+        <tr>
+            <td><a href="/">大東流合気武道会 – Daito Ryu Aikibudokai</a></td>
+            <td>
+                <p>Instructor...</p>
+                <p>Credentials...</p>
+            </td>
+            <td>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Date...</td>
+                            <td>Time...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+            <td>Cost...</td>
+            <td>Region<br />Place<br/>Address</td>
+        </tr>
+
+    </tbody>
+</table>
