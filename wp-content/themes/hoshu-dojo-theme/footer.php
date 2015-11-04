05830c0b882fbafc10218589b026a01d71be2a15
Jeff Kamo <kamojeffrey@gmail.com>
2015-11-03 23:01:55 -0800

    Add newsletter subscribe to footer

--- wp-content/themes/hoshu-dojo-theme/footer.php
+++ wp-content/themes/hoshu-dojo-theme/footer.php
@@ -30,6 +30,57 @@
             </div>
         </nav>
 
+        <div class="c-footer__newsletter">
+            <!-- Begin MailChimp Signup Form -->
+            <!--link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css"-->
+            <div id="mc_embed_signup">
+                <form action="//hoshudojo.us9.list-manage.com/subscribe/post?u=932de532d91ec18ef1f144adf&amp;id=f13bddc5a9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <h2 class="c-h3">Subscribe to our mailing list</h2>
+
+                    <div class="c-field-row">
+                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_932de532d91ec18ef1f144adf_f13bddc5a9" tabindex="-1" value=""></div>
+
+                        <div class="c-field mc-field-group">
+                            <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                        </div>
+
+                        <div class="c-field mc-field-group x-first-name">
+                            <label for="mce-FNAME">First Name </label>
+                            <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                        </div>
+
+                        <div class="c-field mc-field-group x-last-name">
+                            <label for="mce-LNAME">Last Name </label>
+                            <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                        </div>
+
+                        <div class="c-field c--shrink clear">
+                            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button c-button">
+                        </div>
+                    </div>
+
+                    <div class="c-field-row">
+                        <div class="c-field indicates-required">
+                            <div class="c-label">
+                                <span class="asterisk">*</span> indicates required
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="c-field-row">
+                        <div id="mce-responses" class="clear">
+                            <div class="response" id="mce-error-response" style="display:none"></div>
+                            <div class="response" id="mce-success-response" style="display:none"></div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+            <!--End mc_embed_signup-->
+        </div>
+
         <div class="c-footer__info" role="contentinfo">
             <div class="c-text-content c--padded">
                 <div class="c-frame">
