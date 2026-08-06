e4d0ac51c5cf5bbbd73bdeb61464edf5d4f01b0f
pupocalypse <pupocalypse@users.noreply.github.com>
2026-08-06 00:38:47 +0000

    deploy: 64399ecc34e54e7009c90f3e4ecb1dc387198d56

---
+++ scripts/events.js
@@ -0,0 +1,51 @@
+const subNav = document.getElementById("subnav");
+const subNavItems = subNav.querySelectorAll("a");
+const subNavLinks = subNav.querySelectorAll("li > a");
+
+const openSubNav = () => {
+	subNav.setAttribute("data-open", "true");
+	subNav.setAttribute("aria-expanded", "true");
+
+	for (const item of subNavItems) {
+		item.setAttribute("aria-hidden", "false");
+	}
+};
+
+const closeSubNav = () => {
+	subNav.setAttribute("data-open", "false");
+	subNav.setAttribute("aria-expanded", "false");
+
+	for (const item of subNavItems) {
+		item.setAttribute("aria-hidden", "true");
+	}
+};
+
+const toggleSubNav = (e) => {
+	e.stopPropagation();
+
+	const isOpen = subNav.getAttribute("data-open") === "true";
+
+	isOpen ? closeSubNav() : openSubNav();
+};
+
+const handleBodyClick = (e) => {
+	const isInSubNav = subNav.contains(e.target);
+
+	if (!isInSubNav) {
+		closeSubNav();
+	}
+};
+
+subNav.addEventListener("click", toggleSubNav);
+document.addEventListener("click", handleBodyClick);
+document.addEventListener("keydown", (e) => {
+	const navIsOpen = subNav.getAttribute("data-open") === "true";
+
+	if (navIsOpen && e.key === "Escape") {
+		toggleMenu();
+	}
+});
+
+for (const item of subNavItems) {
+	item.addEventListener("click", closeSubNav);
+}
